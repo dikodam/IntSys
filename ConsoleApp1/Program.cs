59edd2ba3adcb73c344b35951afa4e44b8517620
Jonas Mai <jo@jonasmai.de>
2017-10-04 19:32:59 +0200

    cleanup und kommentare

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 
 namespace ConsoleApp1
 {
+    // Aufgabe 1: Geeigneter Delegate
     public delegate void RechenOperation(double a, double b);
 
     internal class Program
@@ -9,31 +10,45 @@ namespace ConsoleApp1
         private static void Main(string[] args)
         {
             RechenOperation meineOperation = null;
-            try
-            {
-                Console.Write("Ersten Operand: ");
-                var operand1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Zweiten Operand: ");
-                var operand2 = Convert.ToDouble(Console.ReadLine());
+            var operand1 = RequestUserInput("Ersten Operand: ");
+            var operand2 = RequestUserInput("Zweiten Operand: ");
+            
+            // Aufgabe 2: Erstelle Multicast Delegaten...
+            // ...via Operatoren
+            meineOperation += Addiere;
+            meineOperation += Multipliziere;
 
-                meineOperation += Addiere;
-                meineOperation += Multipliziere;
+            // ...via Methode der Klasse Delegate
+            RechenOperation add = Addiere;
+            RechenOperation mult = Multipliziere;
+            Delegate[] delArr = { meineOperation, add, mult };
+            meineOperation = (RechenOperation)Delegate.Combine(delArr);
 
-                RechenOperation add = Addiere;
-                RechenOperation mult = Multipliziere;
-                Delegate[] delArr = { meineOperation, add, mult };
-                meineOperation = (RechenOperation)Delegate.Combine(delArr);
+            // ...via Anonyme Methode
+            meineOperation += ((double a, double b) => { Console.WriteLine($"Summe:   {a} + {b} = {a + b}"); });
+            meineOperation += ((double a, double b) => { Console.WriteLine($"Produkt: {a} * {b} = {a * b}"); });
 
-                meineOperation += ((double a, double b) => { Console.WriteLine(a + b); });
-                meineOperation += ((double a, double b) => { Console.WriteLine(a * b); });
+            meineOperation(operand1, operand2);
+            Console.ReadLine();
 
-                meineOperation(operand1, operand2);
-                Console.ReadLine();
-            }
-            catch (FormatException) { Console.WriteLine("Falsche Eingabe"); }
         }
 
-        private static void Addiere(double op1, double op2) { Console.WriteLine(op1 + op2); }
-        private static void Multipliziere(double op1, double op2) { Console.WriteLine(op1 * op2); }
+        private static double RequestUserInput(string askFor)
+        {
+            double operand = Double.MinValue;
+            try
+            {
+                Console.Write(askFor);
+                operand = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException) {
+                Console.WriteLine("Falsche Eingabe");
+                Environment.Exit(0);
+            }
+            return operand;
+        }
+
+        private static void Addiere(double op1, double op2) { Console.WriteLine($"Summe:   {op1} + {op2} = {op1 + op2}"); }
+        private static void Multipliziere(double op1, double op2) { Console.WriteLine($"Produkt: {op1} * {op2} = {op1 * op2}"); }
     }
 }
